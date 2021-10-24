ffcf6edbb2959bf370ef3142d11e8576c18460aa
gregorio1297 <gregorio.guzman17@tectijuana.edu.mx>
2021-10-24 04:07:55 -0700

    Creacion de pantalla principal, login y registro

--- Proyecto/app/src/main/java/com/example/midoctor/MainActivity.java
+++ Proyecto/app/src/main/java/com/example/midoctor/MainActivity.java
@@ -2,13 +2,32 @@ package com.example.midoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button mLoginButton;
+
+    public void SiguienteLog(View view){
+        Intent siguiente=new Intent(this,LoginActivity.class);
+        startActivity(siguiente);
+    }
+
+    public void SiguienteReg(View view){
+        Intent siguiente=new Intent(this,RegisterActivity.class);
+        startActivity(siguiente);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
     }
+
+
 }
