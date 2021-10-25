21329c432be9a58269b4ee01158c92b5073f1953
gregorio1297 <gregorio.guzman17@tectijuana.edu.mx>
2021-10-25 04:06:34 -0700

    Creacion de pantalla principal

--- Proyecto/app/src/main/java/com/example/midoctor/LoginActivity.java
+++ Proyecto/app/src/main/java/com/example/midoctor/LoginActivity.java
@@ -2,9 +2,17 @@ package com.example.midoctor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    public void SiguientePrincipal(View view){
+        Intent siguiente=new Intent(this,PrincipalActivity.class);
+        startActivity(siguiente);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
